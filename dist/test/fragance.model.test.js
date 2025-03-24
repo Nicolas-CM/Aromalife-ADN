9eed6eed1896e7dbfd470294ccb911858b04fc32
Davide Flamini Cazaran <davidecazaran@gmail.com>
2025-03-24 16:46:19 -0500

    test-fix: fragance and container

--- dist/test/fragance.model.test.js
+++ dist/test/fragance.model.test.js
@@ -10,26 +10,36 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fragrance_model_1 = require("../models/fragrance.model");
-describe("Fragrance Model", () => {
-    it("debe guardar nombre, precio y color", () => __awaiter(void 0, void 0, void 0, function* () {
-        const fragrance = new fragrance_model_1.FragranceModel({
-            name: "Azul",
-            color: "Azul",
-            price: 15000,
-        });
-        const fraganceMongo = yield fragrance.save();
-        expect(fraganceMongo._id).toBeDefined();
-        expect(fraganceMongo.name).toBe("Azul");
-        expect(fraganceMongo.color).toBe("Azul");
-        expect(fraganceMongo.price).toBe(15000);
-    }));
-    it("debe guardar color en formato hexadecimal", () => __awaiter(void 0, void 0, void 0, function* () {
-        const fragrance = new fragrance_model_1.FragranceModel({
-            name: "Azul",
-            color: "#0000FF",
-            price: 15000,
-        });
-        const error = fragrance.validateSync();
-        expect(error).toBeUndefined();
-    }));
-});
+it("debe guardar color en formato hexadecimal", () => __awaiter(void 0, void 0, void 0, function* () {
+    const fragrance = new fragrance_model_1.FragranceModel({
+        name: "Azul",
+        color: "#0000FF",
+        price: 15000,
+    });
+    const error = fragrance.validateSync();
+    expect(error).toBeUndefined();
+}));
+it("debe requerir precio ", () => __awaiter(void 0, void 0, void 0, function* () {
+    const fragrance = new fragrance_model_1.FragranceModel({
+        name: "Rojo",
+        color: "#FF0000",
+    });
+    const error = fragrance.validateSync();
+    expect(error === null || error === void 0 ? void 0 : error.errors.price).toBeDefined();
+}));
+it("debe requerir nombre ", () => __awaiter(void 0, void 0, void 0, function* () {
+    const fragrance = new fragrance_model_1.FragranceModel({
+        color: "#FF0000",
+        price: 20000,
+    });
+    const error = fragrance.validateSync();
+    expect(error === null || error === void 0 ? void 0 : error.errors.name).toBeDefined();
+}));
+it("debe requerir color ", () => __awaiter(void 0, void 0, void 0, function* () {
+    const fragrance = new fragrance_model_1.FragranceModel({
+        name: "Verde",
+        price: 20000,
+    });
+    const error = fragrance.validateSync();
+    expect(error === null || error === void 0 ? void 0 : error.errors.color).toBeDefined();
+}));
--- src/test/fragance.model.test.ts
+++ src/test/fragance.model.test.ts
@@ -1,27 +1,42 @@
 import { FragranceModel } from "../models/fragrance.model";
 
-describe("Fragrance Model", () => {
-  it("debe guardar nombre, precio y color", async () => {
-    const fragrance = new FragranceModel({
-      name: "Azul",
-      color: "Azul",
-      price: 15000,
-    });
-    const fraganceMongo = await fragrance.save();
-    expect(fraganceMongo._id).toBeDefined();
-    expect(fraganceMongo.name).toBe("Azul");
-    expect(fraganceMongo.color).toBe("Azul");
-    expect(fraganceMongo.price).toBe(15000);
+it("debe guardar color en formato hexadecimal", async () => {
+  const fragrance = new FragranceModel({
+    name: "Azul",
+    color: "#0000FF",
+    price: 15000,
   });
 
-  it("debe guardar color en formato hexadecimal", async () => {
-    const fragrance = new FragranceModel({
-      name: "Azul",
-      color: "#0000FF",
-      price: 15000,
-    });
+  const error = fragrance.validateSync();
+  expect(error).toBeUndefined();
+});
 
-    const error = fragrance.validateSync();
-    expect(error).toBeUndefined();
+it("debe requerir precio ", async () => {
+  const fragrance = new FragranceModel({
+    name: "Rojo",
+    color: "#FF0000",
   });
+
+  const error = fragrance.validateSync();
+  expect(error?.errors.price).toBeDefined();
+});
+
+it("debe requerir nombre ", async () => {
+  const fragrance = new FragranceModel({
+    color: "#FF0000",
+    price: 20000,
+  });
+
+  const error = fragrance.validateSync();
+  expect(error?.errors.name).toBeDefined();
+});
+
+it("debe requerir color ", async () => {
+  const fragrance = new FragranceModel({
+    name: "Verde",
+    price: 20000,
+  });
+
+  const error = fragrance.validateSync();
+  expect(error?.errors.color).toBeDefined();
 });
